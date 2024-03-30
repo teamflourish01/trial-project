cff7a7416c0834a2f192eaa26eafe47722bd2ac3
ahirharpal <harpalahir0807@gmail.com>
2024-03-30 11:58:34 +0530

    add all

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './Navbar'
+import Navbar from './Componts/Navbar'
 
 const App = () => {
   return (
--- frontend/src/Componts/Navbar.jsx
+++ frontend/src/Componts/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Style/Navbar.css'
+import '../Style/Navbar.css'
 
 const Navbar = () => {
   return (
