e854bcb188e11cc5d845677a291f87b8673eedd7
satyen mori <satyenmori@gmail.com>
2024-03-30 15:33:55 +0530

    Fronted Car Add Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-
+import React from "react";
+import Navbar from "./Components/Navbar";
+import CardetailForm from "./Components/Cardetail-Form";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/caradd" element={<CardetailForm />} />
+        </Routes>
+        <CardetailForm />
+      </BrowserRouter>
+    </>
+  );
+};
 
-    <div>
-      <Navbar />
-
-    </div>
-  )
-}
-
-export default App
+export default App;
